a161604c86f607d5a04a5bcee5026440ff0bba92
Hamza Rabah <h.rabah@outlook.com>
2023-07-26 02:18:29 +0400

    generate embed url

--- app/api/route.ts
+++ app/api/route.ts
@@ -4,14 +4,38 @@ export async function GET() {
     return NextResponse.json("This is a new API route");
 }
 
+// base URL of your Retool instance + https://
+const retoolURL = "https://apaleo.retool.com";
+// key you generate in Settings > API in Retool
+const secret_key = process.env.RETOOL_API_KEY ?? "";
+const app_uuid = "b0e9156c-2b0a-11ee-b939-7b6b556284d2";
+
 export async function POST() {
     const headers: HeadersInit = {
-        'Content-Type': 'application/json', 'API-Key': process.env.DATA_API_KEY ?? ""
+        'Content-Type': 'application/json', // The RETOOL_API_KEY is the token generated in the first step
+        Authorization: `Bearer ${secret_key}`
     };
-    const res = await fetch('https://httpdump.app/dumps/30b1fff4-70f9-434b-abea-acedb31ec63d', {
-        method: 'POST', headers: headers, body: JSON.stringify({time: new Date().toISOString()}),
-    })
 
-
-    return NextResponse.json({embedUrl: "https://retoolin.tryretool.com/embedded/public/f7607e1f-670a-4ebf-9a09-be54cf17181e"})
+    // all parameters can be found here: https://docs.retool.com/docs/retool-embed-quickstart#post-request-parameters
+    const body = {
+        landingPageUuid: app_uuid, // IDs of the groups you want the user to belong to - array of IDs
+        groupIds: [1865667], // external identifier - typically email address
+        externalIdentifier: "123", userInfo: {
+            firstName: "", lastName: "", email: "hamza.rabah@apaleo.com",
+        }, metadata: {
+            msg: "Custom message",
+        },
+    };
+    try {
+        const res = await fetch(`${retoolURL}/api/embed-url/external-user`, {
+            method: 'POST', headers: headers, body: JSON.stringify(body)
+        })
+        const data: any = await res.json();
+        console.log("route log ===============");
+        console.log({data});
+        return NextResponse.json({embedUrl: data["embedUrl"] ?? "https://retoolin.tryretool.com/embedded/public/f7607e1f-670a-4ebf-9a09-be54cf17181e"});
+    } catch (error) {
+        console.log({error});
+        return NextResponse.json({error});
+    }
 }
