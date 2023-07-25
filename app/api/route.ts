a7dcd792147eb75fafa61fd9988a79376febafdd
Hamza Rabah <h.rabah@outlook.com>
2023-07-25 22:36:52 +0400

    fix typing issue

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,14 +1,18 @@
 import {NextResponse} from 'next/server'
 
-export async function GET(Request) {
+export async function GET() {
     return NextResponse.json("This is a new API route");
 }
 
 export async function POST() {
+    const headers: HeadersInit = {
+        'Content-Type': 'application/json',
+        'API-Key': process.env.DATA_API_KEY ?? ""
+    };
     const res = await fetch('https://httpdump.app/dumps/30b1fff4-70f9-434b-abea-acedb31ec63d', {
-        method: 'POST', headers: {
-            'Content-Type': 'application/json', 'API-Key': process.env.DATA_API_KEY,
-        }, body: JSON.stringify({time: new Date().toISOString()}),
+        method: 'POST',
+        headers: headers
+        , body: JSON.stringify({time: new Date().toISOString()}),
     })
 
     const data = await res.json()
